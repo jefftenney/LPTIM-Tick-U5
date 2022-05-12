11811a4b8212b3829b456e87bf08cfbe280881f3
Jeff Tenney <jeff.tenney@gmail.com>
2022-05-12 16:37:36 -0700

    Use medium-high drive on LSE
    Errata now prohibits use of low and medium low drive settings.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -386,7 +386,7 @@ void SystemClock_Config(void)
   /** Configure LSE Drive Capability
   */
   HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_MEDIUMLOW);
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_MEDIUMHIGH);
   /** Initializes the CPU, AHB and APB busses clocks
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
